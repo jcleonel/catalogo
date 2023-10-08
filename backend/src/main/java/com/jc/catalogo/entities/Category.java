538e71f934e0d2af59ea86ac1f50c0101a9c3230
Jean Leonel <jean.leonel.costa@gmail.com>
2023-10-08 17:15:03 -0300

    Auditing data

--- backend/src/main/java/com/jc/catalogo/entities/Category.java
+++ backend/src/main/java/com/jc/catalogo/entities/Category.java
@@ -1,13 +1,11 @@
 package com.jc.catalogo.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.time.Instant;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,5 +21,23 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Getter
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    @Getter
+    private Instant updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = Instant.now();
+    }
 
 }
