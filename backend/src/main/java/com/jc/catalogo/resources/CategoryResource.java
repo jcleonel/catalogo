3fb526ca513d5d85752cfd6b9dc11100e3ff6910
Jean Leonel <jean.leonel.costa@gmail.com>
2023-10-08 17:29:27 -0300

    Pagination Category

--- backend/src/main/java/com/jc/catalogo/resources/CategoryResource.java
+++ backend/src/main/java/com/jc/catalogo/resources/CategoryResource.java
@@ -2,12 +2,14 @@ package com.jc.catalogo.resources;
 
 import com.jc.catalogo.dto.CategoryDTO;
 import com.jc.catalogo.services.CategoryService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
@@ -20,8 +22,16 @@ public class CategoryResource {
     }
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll() {
-        return ResponseEntity.ok().body(categoryService.findAll());
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> categoryDTOList = categoryService.findAllPaged(pageRequest);
+
+        return ResponseEntity.ok().body(categoryDTOList);
     }
 
     @GetMapping(value = "/{id}")
@@ -43,7 +53,7 @@ public class CategoryResource {
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO ) {
+    public ResponseEntity<CategoryDTO> update(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
         categoryDTO = categoryService.update(id, categoryDTO);
         return ResponseEntity.ok().body(categoryDTO);
     }
